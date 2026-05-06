7754b68cb495b0abef7bbc5b1df426cea0d54ec6
Akash A S <akashsumesh2004@gmail.com>
2026-05-06 23:21:52 +0530

    consoled translate error

--- apiServer/lib/translate.js
+++ apiServer/lib/translate.js
@@ -3,14 +3,16 @@ import axios from "axios";
 
 export async function translate(text, target) {
   const url = "https://translation.googleapis.com/language/translate/v2";
-
-  const res = await axios.post(url, {}, {
-    params: {
-      q: text,
-      target,
-      key: process.env.API_KEY
-    }
-  });
-
+  try {
+    const res = await axios.post(url, {}, {
+      params: {
+        q: text,
+        target,
+        key: process.env.API_KEY
+      }
+    })
+  } catch (error) {
+    console.log("ERROR:", err.response?.data || err.message);
+  }
   return res.data.data.translations[0].translatedText;
 }
